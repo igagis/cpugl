5376181b12bb48ad090314da6e03793a5546bf09
Ivan Gagis <ivan.gagis@ge.com>
2023-09-08 15:04:51 +0300

    stuff

---
+++ tests/app/src/main.cpp
@@ -0,0 +1,117 @@
+#include <chrono>
+
+#include <utki/debug.hpp>
+#include <utki/config.hpp>
+#include <utki/time.hpp>
+
+#include <papki/fs_file.hpp>
+
+#include <rasterimage/image_variant.hpp>
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#if M_OS == M_OS_LINUX
+#	include <X11/Xlib.h>
+#	include <X11/Xutil.h>
+#endif
+
+#ifdef assert
+#	undef assert
+#endif
+
+// NOLINTNEXTLINE(bugprone-exception-escape): fatal exceptions are not caught
+int main(int argc, char **argv){
+#ifdef DEBUG
+	// auto loadStart = utki::get_ticks_ms();
+#endif
+	
+	// auto dom = svgdom::load(papki::fs_file(filename));
+	// utki::assert(dom, SL);
+	
+	// LOG([&](auto&o){o << "SVG loaded in " << float(utki::get_ticks_ms() - loadStart) / 1000.0f << " sec." << std::endl;})
+	
+	// auto render_start_ms = utki::get_ticks_ms();
+	
+	// auto image = rasterimage::image_variant(svgren::rasterize(*dom));
+
+	// const auto& img = image.get<rasterimage::format::rgba>();
+	
+	// NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+	// utki::log([&](auto&o){o << "SVG rendered in " << float(utki::get_ticks_ms() - render_start_ms) / 1000.0f << " sec." << std::endl;});
+	
+	// utki::log([&](auto&o){o << "img.dims = " << img.dims() << " img.pixels.size() = " << img.pixels().size() << std::endl;});
+
+	// image.write_png(papki::fs_file(out_filename));
+	
+#if M_OS == M_OS_LINUX
+	// auto width = int(img.dims().x() + 2);
+	// auto height = int(img.dims().y() + 2);
+	auto width = 800;
+	auto height = 600;
+
+	Display *display = XOpenDisplay(nullptr);
+	
+	// NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast, cppcoreguidelines-pro-bounds-pointer-arithmetic)
+	Visual *visual = DefaultVisual(display, 0);
+	
+	// NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast, cppcoreguidelines-pro-bounds-pointer-arithmetic)
+	Window window = XCreateSimpleWindow(display, RootWindow(display, 0), 0, 0, width, height, 1, 0, 0);
+	
+	if(visual->c_class != TrueColor){
+		utki::log([](auto&o){o << "Cannot handle non true color visual ...\n" << std::endl;});
+		return 1;
+	}	
+	
+	XSelectInput(display, window, ButtonPressMask|ExposureMask|KeyPressMask);
+	
+	XMapWindow(display, window);
+	
+	while(true){
+		XEvent ev;
+		XNextEvent(display, &ev);
+		switch(ev.type){
+			default:
+				break;
+			case Expose:
+				{
+					int dummy_int = 0;
+					unsigned dummy_unsigned = 0;
+					Window dummy_window = 0;
+					unsigned win_width = 0;
+					unsigned win_height = 0;
+					
+					XGetGeometry(display, window, &dummy_window, &dummy_int, &dummy_int, &win_width, &win_height, &dummy_unsigned, &dummy_unsigned);
+										
+					// using std::max;
+					
+					// svgren::parameters p;
+					// p.dpi = svgren::parameters::default_dpi;
+					// p.dims_request.x() = max(int(win_width) - 2, 0);
+					// p.dims_request.y() = max(int(win_height) - 2, 0);
+					// auto img = svgren::rasterize(*dom, p);
+
+					// img.swap_red_blue();
+
+					// NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+					// auto ximage = XCreateImage(display, visual, utki::byte_bits * 3, ZPixmap, 0, reinterpret_cast<char*>(img.pixels().data()), img.dims().x(), img.dims().y(), utki::byte_bits, 0);
+					// utki::scope_exit scope_exit([ximage](){
+					// 	ximage->data = nullptr; // Xlib will try to deallocate data which is owned by 'img' variable.
+					// 	XDestroyImage(ximage);
+					// });
+					
+					// // NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast, cppcoreguidelines-pro-bounds-pointer-arithmetic)
+					// XPutImage(display, window, DefaultGC(display, 0), ximage, 0, 0, 1, 1, img.dims().x(), img.dims().y());
+				}
+				break;
+			case KeyPress:
+			case ButtonPress:
+				exit(0);
+				break;
+		}
+	}
+#endif
+
+	utki::log([](auto&o){o << "[PASSED]" << std::endl;});
+}

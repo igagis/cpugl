bc7401cec39f1518c5842cfb0a689f3cee2e1b74
Ivan Gagis <igagis@gmail.com>
2023-09-30 23:34:45 +0300

    stuff

--- src/cpugl/context.hpp
+++ src/cpugl/context.hpp
@@ -28,12 +28,31 @@ SOFTWARE.
 
 #include <rasterimage/image.hpp>
 
+#include <r4/segment2.hpp>
+
 #include "config.hpp"
 
 namespace cpugl {
 
 class context
 {
+	static r4::segment2<real> calc_bounding_box_segment(
+		const r4::vector2<real>& v0,
+		const r4::vector2<real>& v1,
+		const r4::vector2<real>& v2
+	)
+	{
+		using std::min;
+		using std::max;
+
+		return {min(v0, min(v1, v2)), max(v0, max(v1, v2))};
+	}
+
+	static real edge_function(const r4::vector2<real>& a, const r4::vector2<real>& b, const r4::vector2<real>& c)
+	{
+		return (c - a).cross(b - a);
+	}
+
 public:
 	using fb_image_type = rasterimage::image<uint8_t, 4>;
 
@@ -54,7 +73,46 @@ public:
 		this->framebuffer->span().clear(color);
 	}
 
-	void render(utki::span<const r4::vector4<real>> pos);
+	// template <typename vertex_program_type, typename fragment_program_type, bool depth_test = false>
+	void render(utki::span<const r4::vector4<real>> pos)
+	{
+		std::array<r4::vector4<real>, 3> face{};
+		auto face_i = face.begin();
+		for (const auto& vertex : pos) {
+			*face_i = vertex;
+			++face_i;
+			if (face_i != face.end()) {
+				continue;
+			}
+			face_i = face.begin();
+
+			auto bb_segment = calc_bounding_box_segment(face[0], face[1], face[2]);
+
+			r4::rectangle<real> bb = {
+				bb_segment.p1,
+				bb_segment.p2 - bb_segment.p1 + decltype(bb_segment.p1)(1) // +1 because segment is [p1, p2]
+			};
+
+			auto framebuffer_span = this->framebuffer->span().subspan(bb.to<unsigned>());
+
+			auto p = bb.p;
+			for (auto line : framebuffer_span) {
+				for (auto& px : line) {
+					auto w0 = edge_function(face[1], face[2], p);
+					auto w1 = edge_function(face[2], face[0], p);
+					auto w2 = edge_function(face[0], face[1], p);
+
+					if (w0 >= 0 && w1 >= 0 && w2 >= 0) {
+						px = {0, 0xff, 0, 0xff}; // NOLINT
+					}
+
+					++p.x();
+				}
+				p.x() = bb.p.x();
+				++p.y();
+			}
+		}
+	}
 };
 
 } // namespace cpugl

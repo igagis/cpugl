1ee39265383963bf66b1ddbc2e8c821b385156cd
Ivan Gagis <ivan.gagis@ge.com>
2023-10-27 13:17:19 +0300

    stuff

--- src/cpugl/shaders/texture_pos_tex_shader.cpp
+++ src/cpugl/shaders/texture_pos_tex_shader.cpp
@@ -1,5 +1,8 @@
 #include "texture_pos_tex_shader.hpp"
 
+#include "../texture.hpp"
+#include "../pipeline.hpp"
+
 using namespace cpugl;
 
 void texture_pos_tex_shader::render( //
@@ -9,5 +12,25 @@ void texture_pos_tex_shader::render( //
 	const mesh<tex_coord_type>& mesh
 )
 {
-	// TODO:
+	std::visit(
+		[&ctx, &matrix, &mesh](const auto& image) {
+			if constexpr (std::is_same_v<uint8_t, typename std::remove_reference_t<decltype(image)>::value_type>) {
+                auto tex = make_texture(image);
+
+				pipeline::render<false>(
+                    ctx,
+                    [&matrix](const r4::vector4<real>& pos, const r4::vector2<real> tex_coord){
+                        return std::make_tuple(matrix * pos, tex_coord);
+                    },
+                    [&tex](const r4::vector2<real>& tex_coord){
+                        return tex.get(tex_coord);
+                    },
+                    mesh
+                );
+			} else {
+				std::cout << "texture_pos_tex_shader::render(): non-uint8_t textures are not supported" << std::endl;
+			}
+		},
+		tex.variant
+	);
 }
--- src/cpugl/texture.hpp
+++ src/cpugl/texture.hpp
@@ -7,23 +7,31 @@ namespace cpugl {
 template <typename image_type>
 class texture
 {
-	static_assert(
-		utki::is_specialization_of_v<rasterimage::image, image_type>,
-		"image_type must be a specialization of rasterimage::image"
-	);
+	// NOTE: utki::is_specialization_of_v doesn't work for non-type templates
+	// static_assert(
+	// 	utki::is_specialization_of_v<rasterimage::image, image_type>,
+	// 	"image_type must be a specialization of rasterimage::image"
+	// );
 
-	image_type image;
-	r4::vector2<cpugl::real> dims;
+	const image_type& image;
+	r4::vector2<real> dims;
 
 public:
-	texture(image_type image) :
-		image(std::move(image))
+	texture(const image_type& image) :
+		image(image),
+		dims(image.dims().template to<real>())
 	{}
 
-	const image_type::pixel_type& get(const r4::vector2<cpugl::real>& tex_coords) const
+	const image_type::pixel_type& get(const r4::vector2<real>& tex_coords) const
 	{
 		// TODO:
+		return image[0][0];
 	}
 };
+
+template <typename image_type>
+texture<image_type> make_texture(const image_type& image){
+	return texture<image_type>(image);
+}
 
 } // namespace cpugl
